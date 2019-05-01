67669e25ee6cfe372fa7ef59573abd3af911a747
Bryna Dunnells <badunnells@gmail.com>
2019-05-01 02:10:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,17 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-    if   num % 5 == 0
-     "Buzz"
+    if  num % 3 == 0 && num % 5 == 0
+          "FizzBuzz"
+
+
+    
 
     elsif num % 3 == 0
    "Fizz"
 
- elsif num % 3 == 0 && num % 5 == 0
-       "FizzBuzz"
-
+ elsif    num % 5 == 0
+  "Buzz"
 
 
   else
