3bf1363a816cbd663d53e862f4813b4c829a0cb8
Bryna Dunnells <badunnells@gmail.com>
2019-05-01 01:46:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(num)
+    if num/3 == 0
+      puts "Fizz"
 
+    elsif num/5 == 0
+      puts "Buzz"
+
+    elsif num/2 == 0
+      puts "FizzBuzz"
+
+    else 
+      return "nil"
+      
 
 end
